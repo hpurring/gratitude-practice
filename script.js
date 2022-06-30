dc0f2cd0b0e99028a84df71cdf92bf858e95b6ff
hpurring <hilarypurrington@gmail.com>
2022-06-29 20:13:39 -0400

    fixed saved copies

--- script.js
+++ script.js
@@ -6,72 +6,77 @@ var savedContainer = document.getElementById("saved-container");
 
 var savedText = [];
 
-var getQuestion = function() {
+var getQuestion = function () {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "5f4b8fcd5dmsh8a2b9b8d6ea7074p1cc2c7jsn81d09102e3c1",
+      "X-RapidAPI-Host": "gratitude-questions.p.rapidapi.com",
+    },
+  };
 
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '5f4b8fcd5dmsh8a2b9b8d6ea7074p1cc2c7jsn81d09102e3c1',
-            'X-RapidAPI-Host': 'gratitude-questions.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://gratitude-questions.p.rapidapi.com/question', options)
-    .then(function(response) {
-        // request was successful
-        if (response.ok) {
-            console.log(response);
-            response.json().then(function(data) {
-                console.log(data);
-                displayQuestion(data);
-                displayResponse();
-            });
-            } else {
-                alert('Error: ' + response.statusText);
-                }
-            })
-            .catch(function(error) {
-            alert('Unable to connect.');
-            });
+  fetch("https://gratitude-questions.p.rapidapi.com/question", options)
+    .then(function (response) {
+      // request was successful
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          displayQuestion(data);
+          displayResponse();
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect.");
+    });
 };
 
-var displayQuestion = function(data) {
-    questionEl.innerHTML = data.question;
+var displayQuestion = function (data) {
+  questionEl.innerHTML = data.question;
 };
 
-var displayResponse = function() {
-    responseEl.style.display = 'block';
-    saveBtn.style.display = 'block';
-}
+var displayResponse = function () {
+  responseEl.style.display = "block";
+  saveBtn.style.display = "block";
+};
 
 saveBtn.addEventListener("click", function () {
-    var savedEntries = JSON.parse(localStorage.getItem("gratitude"));
-        if (!savedEntries) {
-            savedText.push(responseEl.value);        
-            localStorage.setItem("gratitude", JSON.stringify(savedText));
-        } else {
-            savedEntries = JSON.parse(localStorage.getItem("gratitude"));
-            savedText = savedEntries;
-            savedText.push(responseEl.value);        
-            localStorage.setItem("gratitude", JSON.stringify(savedText));
-        };
-        console.log("Gratitude entry saved.");
-        responseEl.value = "";
-        showSaved();
-        getQuestion();
+  var savedEntries = JSON.parse(localStorage.getItem("gratitude"));
+  if (!savedEntries) {
+    savedText.push(responseEl.value);
+    localStorage.setItem("gratitude", JSON.stringify(savedText));
+  } else {
+    savedEntries = JSON.parse(localStorage.getItem("gratitude"));
+    savedText = savedEntries;
+    savedText.push(responseEl.value);
+    localStorage.setItem("gratitude", JSON.stringify(savedText));
+  }
+  console.log("Gratitude entry saved.");
+  responseEl.value = "";
+  showSaved();
+  getQuestion();
 });
 
-var showSaved = function() {
-    var savedEntry = JSON.parse(localStorage.getItem("gratitude"));
-    if (savedEntry) {
-        for (let x = 0; x < savedEntry.length; x++) {
-            $("#saved-container").append("<a id='saved-item'>" + savedEntry[x] + "</a>")
-        }
-    } else {
-        console.log("Nothing saved yet.");
+var showSaved = function () {
+
+    while (savedContainer.hasChildNodes()) {
+        savedContainer.removeChild(savedContainer.firstChild);
+      }
+    
+  var savedEntry = JSON.parse(localStorage.getItem("gratitude"));
+  if (savedEntry) {
+    for (let x = 0; x < savedEntry.length; x++) {
+      $("#saved-container").append(
+        "<a id='saved-item'>" + savedEntry[x] + "</a>"
+      );
     }
+  } else {
+    console.log("Nothing saved yet.");
+  }
 };
 
-
 var buttonEl = document.getElementById("question-button");
-buttonEl.addEventListener('click', getQuestion);
+buttonEl.addEventListener("click", getQuestion);
